152cb62424c650fecc08c0425c69be5de202e9c3
Zimo Li <7163127+lzm0@users.noreply.github.com>
2024-05-24 16:38:10 -0400

    Replace CSS vendor prefix for `transform`

--- projects/swimlane/ngx-datatable/src/lib/utils/translate.ts
+++ projects/swimlane/ngx-datatable/src/lib/utils/translate.ts
@@ -2,7 +2,7 @@ import { getVendorPrefixedName } from './prefixes';
 import { camelCase } from './camel-case';
 
 // browser detection and prefixing tools
-const transform = typeof window !== 'undefined' ? getVendorPrefixedName('transform') : undefined;
+const transform = 'transform';
 const backfaceVisibility = typeof window !== 'undefined' ? getVendorPrefixedName('backfaceVisibility') : undefined;
 const hasCSSTransforms = typeof window !== 'undefined' ? !!getVendorPrefixedName('transform') : undefined;
 const hasCSS3DTransforms = typeof window !== 'undefined' ? !!getVendorPrefixedName('perspective') : undefined;
